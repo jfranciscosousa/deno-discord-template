b589fe7a972a6d84a1ae10fa46f3684da53da207
Francisco Sousa <francisco.sousa@hey.com>
2022-11-20 17:00:07 +0000

    Fix

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { configSync as loadEnv } from "dotenv";
 
 const { DISCORD_APPLICATION_ID, DISCORD_PUBLIC_KEY, DISCORD_BOT_TOKEN } =
-  await loadEnv();
+  typeof Deno.readFileSync !== "undefined" ? loadEnv() : Deno.env.toObject();
 
 const config = {
   DISCORD_APPLICATION_ID: BigInt(DISCORD_APPLICATION_ID),
