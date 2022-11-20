ee5397224c3079f1125df7d55e6ba43865ee38de
Francisco Sousa <francisco.sousa@hey.com>
2022-11-20 19:45:56 +0000

    Add list of updated guilds to upsert task

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { configSync as loadEnv } from "dotenv";
 
 const { DISCORD_APPLICATION_ID, DISCORD_PUBLIC_KEY, DISCORD_BOT_TOKEN } =
+  // On Deno Deploy we don't have access to some file utilities, in that case we load from `Deno.env`
   typeof Deno.readFileSync !== "undefined" ? loadEnv() : Deno.env.toObject();
 
 const config = {
--- tasks/upsert_commands_task.ts
+++ tasks/upsert_commands_task.ts
@@ -2,8 +2,10 @@ import { getApplicationCommands, upsertApplicationCommands } from "discord";
 import bot from "@/bot.ts";
 import { COMMANDS } from "@/commands/mod.ts";
 
+// Get existing commands
 const existingCommands = (await getApplicationCommands(bot)).array();
 
+// Add the existing ids to existing commands so we update them instead of creating
 const commandsWithId = Object.values(COMMANDS).map((command) => {
   const existingCommandId = existingCommands.find(
     (c) => c.name === command.name
@@ -14,10 +16,13 @@ const commandsWithId = Object.values(COMMANDS).map((command) => {
   return { ...command, id: existingCommandId };
 });
 
+// For each guild, upsert existing commands
 bot.activeGuildIds.forEach(
-  async (guildId) => await upsertApplicationCommands(bot, commandsWithId, guildId)
+  async (guildId) =>
+    await upsertApplicationCommands(bot, commandsWithId, guildId)
 );
 
+console.log(`Updated guilds: ${bot.activeGuildIds.size}`);
 console.log(`New commands: ${commandsWithId.length - existingCommands.length}`);
 console.log(`Updated commands: ${existingCommands.length}`);
 
