60b84d96d4c0241a7334feb4a33189dbebb28376
Francisco Sousa <francisco.sousa@hey.com>
2022-11-20 20:09:10 +0000

    Fix env

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-import { configSync as loadEnv } from "dotenv";
-
 const { DISCORD_APPLICATION_ID, DISCORD_PUBLIC_KEY, DISCORD_BOT_TOKEN } =
-  // On Deno Deploy we don't have access to some file utilities, in that case we load from `Deno.env`
-  typeof Deno.readFileSync !== "undefined" ? loadEnv() : Deno.env.toObject();
+  Deno.env.toObject();
 
 const config = {
   DISCORD_APPLICATION_ID: BigInt(DISCORD_APPLICATION_ID),
