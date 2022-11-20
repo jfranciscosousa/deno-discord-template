ef20c85da689aef2078df46861dbd4c0c0a1a2bf
Francisco Sousa <francisco.sousa@hey.com>
2022-11-20 16:37:22 +0000

    Fix

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-import { config as loadEnv } from "dotenv";
+import { configSync as loadEnv } from "dotenv";
 
 const { DISCORD_APPLICATION_ID, DISCORD_PUBLIC_KEY, DISCORD_BOT_TOKEN } =
-  loadEnv();
+  await loadEnv();
 
 const config = {
   DISCORD_APPLICATION_ID: BigInt(DISCORD_APPLICATION_ID),
