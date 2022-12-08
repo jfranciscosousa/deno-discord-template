964c6b58786670a02dc8018971f0ed50e72945b3
Francisco Sousa <francisco.sousa@hey.com>
2022-12-08 12:10:20 +0000

    Validate env with zod

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { config as dotenv } from "dotenv";
+import { z } from "zod";
 
 function loadEnv() {
   // Deno.readFileSync doesn't exist on serverless contexts
@@ -15,10 +16,24 @@ function loadEnv() {
 const { DISCORD_APPLICATION_ID, DISCORD_PUBLIC_KEY, DISCORD_BOT_TOKEN } =
   loadEnv();
 
-const config = {
+const configSchema = z.object({
+  DISCORD_APPLICATION_ID: z.bigint(),
+  DISCORD_PUBLIC_KEY: z.string(),
+  DISCORD_BOT_TOKEN: z.string(),
+});
+
+const parsedConfig = configSchema.safeParse({
   DISCORD_APPLICATION_ID: BigInt(DISCORD_APPLICATION_ID),
   DISCORD_PUBLIC_KEY,
   DISCORD_BOT_TOKEN,
-};
+});
+
+if (!parsedConfig.success) {
+  console.error("\x1b[31m%s\x1b[0m", "Environment variables error, please review them!")
+  console.error("\x1b[31m%s\x1b[0m", parsedConfig.error)
+  Deno.exit(-1);
+}
+
+const config = parsedConfig.success ? parsedConfig.data : undefined;
 
 export default config;
