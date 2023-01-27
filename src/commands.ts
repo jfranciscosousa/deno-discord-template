cc4477eeef0ab3c245558ab1e3b38d9201f3d6b4
Francisco Sousa <francisco.sousa@hey.com>
2023-01-27 17:03:58 +0000

    Fix builds

--- src/commands.ts
+++ src/commands.ts
@@ -2,8 +2,9 @@ import HELLO_COMMAND from "@/commands/hello.ts";
 import PING_COMMAND from "@/commands/ping.ts";
 import { Command } from "@/commands/utils.ts";
 
-// Add new commands here
+// deno-lint-ignore no-explicit-any
 export const COMMANDS: Record<string, Command<any>> = {
   [PING_COMMAND.name]: PING_COMMAND,
   [HELLO_COMMAND.name]: HELLO_COMMAND,
+  // Add new commands here
 };
