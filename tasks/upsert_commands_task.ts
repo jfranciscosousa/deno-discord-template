6770542795a14279b90f4f0b9ae53f765dfd7228
Francisco Sousa <francisco.sousa@hey.com>
2023-02-26 11:25:38 +0000

    Upgrade packages

--- tasks/upsert_commands_task.ts
+++ tasks/upsert_commands_task.ts
@@ -1,16 +1,27 @@
 import {
-  getApplicationCommands,
+  ApplicationCommand,
+  getGuildApplicationCommands,
+  upsertGuildApplicationCommands,
   startBot,
-  upsertApplicationCommands,
 } from "discord";
 import bot from "@/bot.ts";
 import { COMMANDS } from "@/commands.ts";
 
 await startBot(bot);
 
+async function fetchAllGuildCommands(): Promise<ApplicationCommand[]> {
+  const guildCommands = await Promise.all(
+    Array.from(bot.activeGuildIds).map(async (guildId) =>
+      (await getGuildApplicationCommands(bot, guildId)).array()
+    )
+  );
+
+  return guildCommands.flat();
+}
+
 bot.events.ready = async (_bot, { guilds }) => {
   // Get existing commands
-  const existingCommands = (await getApplicationCommands(bot)).array();
+  const existingCommands = await fetchAllGuildCommands();
 
   // Add the existing ids to existing commands so we update them instead of creating
   const commandsWithId = Object.values(COMMANDS).map((command) => {
@@ -26,7 +37,7 @@ bot.events.ready = async (_bot, { guilds }) => {
   // For each guild, upsert existing commands
   await Promise.all(
     Array.from(guilds).map((guildId) =>
-      upsertApplicationCommands(bot, commandsWithId, guildId)
+      upsertGuildApplicationCommands(bot, guildId, commandsWithId)
     )
   );
 
